f77666f7f0ff1ef1b06a704e030726d956d46afb
ChaoticProject <alexisledesma94@gmail.com>
2019-06-06 16:29:06 -0300

    First com

---
+++ complete/src/main/java/hello/Application.java
@@ -0,0 +1,37 @@
+package hello;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import entyties.User;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import repositories.UserRepository;
+
+@ComponentScan("src")
+@EntityScan("entyties")
+@EnableJpaRepositories("repositories")
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(UserRepository userRepository) {
+        return args -> {
+            Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+                User user = new User(name, name.toLowerCase() + "@domain.com");
+                userRepository.save(user);
+            });
+            userRepository.findAll().forEach(System.out::println);
+        };
+
+}
